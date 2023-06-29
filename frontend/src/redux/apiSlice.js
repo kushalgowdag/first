bd144f089a76a1ddcc7bae398b7da8575a73b338
kushalgowdag <kushalgowda263@gmail.com>
2023-06-29 16:51:10 +0530

    new change

--- frontend/src/redux/apiSlice.js
+++ frontend/src/redux/apiSlice.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react"
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001", credentials: "include" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://evenpole.in:3001", credentials: "include" }),
     tagTypes: ["user", "product"],
     endpoints: builder => ({})
 })
